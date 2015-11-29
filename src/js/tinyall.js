2589837678f4b6556d1bd22f163255e2e058808e
ismael-miguel <espertalhao04@hotmail.com>
2015-11-29 18:30:21 +0000

    Added array, fixed H, added D

--- src/js/tinyall.js
+++ src/js/tinyall.js
@@ -1,5 +1,64 @@
 (function(window, undefined) {
 	var funcs = {
+		//echo
+		'_':function(value, data){
+			var tmp = data.out + (value === undefined ? data.last : value);
+			data.vars[';'] = data.out = tmp;
+			return tmp;
+		},
+		//array
+		A:function(value, data){
+			var split_value = (value || '').split(/(\-?\d+(?:\.\d*)?(?:[eE][\-+]?\d+)?|'(?:\\'|[^']*)*'),?/);
+			var final_value = [];
+			for(var i = 0, l = split_value.length; i<l; i++)
+			{
+				if(split_value[i][0] == '\'')
+				{
+					final_value[final_value.length] = split_value[i].replace(/^'(.*)'$/,'$1').replace(/\\'/g,'\'');
+				}
+				else if(split_value[i]/1 || split_value[i] == '0')
+				{
+					final_value[final_value.length] = +split_value[i];
+				}
+			}
+			
+			return final_value;
+		},
+		//comparison functions
+		C:function(value, data){
+			
+		},
+		D:function(value, data){
+			
+			var debug = {
+				value: value,
+				type: typeof value,
+				memory: data
+			}
+			
+			if(console && console.log)
+			{
+				console.log(debug);
+			}
+			
+			return value;
+		},
+		//eval
+		E:function(value, data){
+			run_code(value, data);
+		},
+		//flip the value around
+		F:function(value, data){
+			if(('object' === typeof value) && value.reverse)
+			{
+				return value.reverse();
+			}
+			else
+			{
+				return value.toString().split('').reverse().join('');
+			}
+		},
+		//hello worlds
 		H:function(value) {
 			return 'hH'[value&1]
 				+'ello'
@@ -10,19 +69,86 @@
 				+(value&8?'!':'')
 				+(value&16?'\n':'');
 		},
-		I:function(value, data){
-			return data.input;
+		//math functions
+		M:function(value, data){
+			
 		},
-		'_':function(value, data){
-			var tmp = data.out + (value === undefined ? data.last : value);
-			data.vars[';'] = data.out = tmp;
+		//not (inverts values/casing)
+		N:function(value) {
+			switch(typeof value)
+			{
+				case 'boolean':
+					return !value;
+				case 'number':
+					return -value;
+				case 'string':
+					var tmp = '';
+					for(var i = 0, l = value.length; i < l; i++)
+					{
+						if(value[i] >= 'a' && value[i] <= 'z')
+						{
+							tmp += String.fromCharCode(value.charCodeAt(i) - 32);
+						}
+						else if(value[i] >= 'A' && value[i] <= 'Z')
+						{
+							tmp += String.fromCharCode(value.charCodeAt(i) + 32);
+						}
+						else
+						{
+							tmp += value[i];
+						}
+					}
+					return tmp;
+				default:
+					return 0;
+			}
+		},
+		//passthrough
+		P:function(value) {
+			return value;
+		},
+		//repeat
+		R:function(value, data){
+			var tmp = '';
+			
+			if(value > 0)
+			{
+				while(value--)
+				{
+					tmp += data.last;
+				}
+			}
+			
 			return tmp;
 		},
-		V:function(value){
-			return get_value(value);
+		//string functions
+		S:function(value, data){
+			var methods = {
+				t:function(value){
+					return value.replace(/^\s+/,'').replace(/\s+$/,'');
+				},
+				u:function(value){
+					return value.toUpperCase();
+				},
+				T:function(value ){
+					
+				},
+				R:function(value){
+					
+				}
+			};
+			
+			var tmp = data.last.toString();
+			for(var i = 0, l = value.length; i < l; i++)
+			{
+				tmp = value[i] in methods ? methods[value[i]](tmp, value, i) : '';
+			}
+		
+			return tmp;
 		},
-		P:function(value){
-			return value;
+		//clears output
+		Z:function(value, data) {
+			return data.vars[';'] = data.out = 0;
 		}
 	};
 	
@@ -37,7 +163,7 @@
 				}
 				else
 				{
-					return get_value(value, data, true)
+					return get_value(value, data, true);
 				}
 			}
 		);
@@ -53,10 +179,10 @@
 		
 		var x = (value || '').toString();
 		
-		if(/^\-?\d+$/.test(x))
+		if(/^\-?\d+(?:\.\d*)?(?:[eE][\-+]?\d+)?$/.test(x))
 		{
 			//returns a number
-			return Function('return ' + x)();
+			return +x;
 		}
 		else if(/^[a-z]$/.test(x))
 		{
@@ -79,6 +205,27 @@
 		}
 	};
 	
+	var run_code = function(code, memory){
+		
+		if(!code || /^[a-z\d]$/.test(code))
+		{
+			memory.out = funcs.H(get_value(code, memory));
+		}
+		else
+		{
+			code.toString().replace(
+				// /(?:([^A-Z_:'"])=)?([A-Z_])(?::('.|"[^"]*"|-?\d+|.))?/g,
+				/(?:([^A-Z_'"])=)?([A-Z_])(?:('.|"[^"]*"|-?\d+|[^A-Z_'"]))?/g,
+				function(_, name, func, value){
+					
+					memory.vars[name || ':'] = memory.last = (funcs[func] || noop)( value ? get_value(value, memory) : memory.last, memory);
+					
+					return '';
+				}
+			);
+		}
+	}
+	
 	var noop = function(){};
 	
 	window.tinyAll=function(code, input) {
@@ -87,27 +234,18 @@
 			last: 0,
 			input: input || 0,
 			vars: {
+				//version
 				'|':'0.3',
+				//input
 				':':input || 0,
-				';':0
+				//output
+				';':0,
+				//code
+				'.':(code || '').toString()
 			}
 		};
 		
-		if(!code || /^[a-z\d]$/.test(code))
-		{
-			return funcs.H(get_value(code, {}));
-		}
-		
-		code.toString().replace(
-			// /(?:([^A-Z_:'"])=)?([A-Z_])(?::('.|"[^"]*"|-?\d+|.))?/g,
-			/(?:([^A-Z_'"])=)?([A-Z_])(?:('.|"[^"]*"|-?\d+|[^A-Z_'"]))?/g,
-			function(_, name, func, value){
-				
-				data.vars[name || ':'] = data.last = (funcs[func] || noop)( value ? get_value(value, data) : data.last, data);
-				
-				return '';
-			}
-		);
+		run_code(data.vars['.'], data);
 		
 		return data.out || 0;
 	};
